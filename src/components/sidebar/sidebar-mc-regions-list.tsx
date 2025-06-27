828db848fedc60ca2f0cf21b950f490dd0d32dba
JurIVoelker <juri.voelker@gmx.de>
2025-06-28 00:13:13 +0200

    fix: reverse sorting order for createdAt and updatedAt in SidebarMcRegionsList

--- src/components/sidebar/sidebar-mc-regions-list.tsx
+++ src/components/sidebar/sidebar-mc-regions-list.tsx
@@ -69,9 +69,9 @@ const SidebarMcRegionsList = ({
     } else if (sortingType === "status") {
       return a.status.localeCompare(b.status);
     } else if (sortingType === "createdAt") {
-      return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+      return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
     } else if (sortingType === "updatedAt") {
-      return new Date(a.updatedAt).getTime() - new Date(b.updatedAt).getTime();
+      return new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime();
     }
     return 0;
   });
